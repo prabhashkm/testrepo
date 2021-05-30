02623d4241630fa93ab1807c4a3d9ac675f124f2
prabhashkm <meherprabhat878@gmail.com>
2021-05-30 10:39:23 +0530

    unwanted test files deleted and test.py modified

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
 #!/usr/local/bin/python
-
 # this is a test python script.
+
+def showmsg():
+    print("You are using GitHub Actions and python packages.")
+
+def main():
+    if __name__ == '__main__':
+        showmsg()
+
